c1bb21251c335dddcac5be9ebe8b8ac6c48cf11c
Lais Omena <laisf.omena@gmail.com>
2022-09-08 22:17:33 +0100

    Adicionando Home e modificando BrowserRouter :rocket:

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes';
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <Routes />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Routes />
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
 
 class Home extends React.Component {
+  state = {
+    search: '',
+  };
+
+  handleSearch = ({ target }) => {
+    const { value } = target;
+    this.setState({ search: value });
+  };
+
   render() {
+    const { search } = this.state;
     return (
-      <div>teste</div>
+      <div>
+        <input
+          type="text"
+          name="search"
+          placeholder="Search"
+          search={ search }
+          onChange={ this.handleSearch }
+        />
+        <div>
+          { search === ''
+          && (
+            <p
+              data-testid="home-initial-message"
+            >
+              Digite algum termo de pesquisa ou escolha uma categoria.
+            </p>
+          )}
+        </div>
+      </div>
     );
   }
 }

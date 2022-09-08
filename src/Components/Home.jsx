4f4594374bb412f239f847bd5b76d8042882d7a6
Lais Omena <laisf.omena@gmail.com>
2022-09-09 00:37:31 +0100

    pegando dados da api e criando card :rocket:

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,26 +1,47 @@
 import React from 'react';
+import { getProductsFromCategoryAndQuery } from '../services/api';
 
 class Home extends React.Component {
   state = {
     search: '',
+    productsApi: [],
   };
+
+  componentDidMount() {
+    this.setState({ productsApi: [] });
+  }
 
   handleSearch = ({ target }) => {
     const { value } = target;
     this.setState({ search: value });
   };
 
-  render() {
+  handleGetData = async () => {
     const { search } = this.state;
+    const response = await getProductsFromCategoryAndQuery(search);
+    console.log('rs', response);
+    this.setState({ productsApi: await response.results });
+  };
+
+  render() {
+    const { search, productsApi } = this.state;
     return (
       <div>
         <input
           type="text"
           name="search"
           placeholder="Search"
+          data-testid="query-input"
           search={ search }
           onChange={ this.handleSearch }
         />
+        <button
+          type="submit"
+          data-testid="query-button"
+          onClick={ this.handleGetData }
+        >
+          Pesquisar
+        </button>
         <div>
           { search === ''
           && (
@@ -30,6 +51,23 @@ class Home extends React.Component {
               Digite algum termo de pesquisa ou escolha uma categoria.
             </p>
           )}
+        </div>
+        <div>
+          <ul>
+            { productsApi.length !== 0 ? 'Nenhum produto foi encontrado'
+              : (
+                productsApi.map((product, index) => (
+                  <li
+                    key={ index }
+                    data-testid="product"
+                  >
+                    <p>{ product.title }</p>
+                    <img src={ product.thumbnail } alt={ product.title } />
+                    <p />
+                    { product.price }
+                  </li>
+                )))}
+          </ul>
         </div>
       </div>
     );
